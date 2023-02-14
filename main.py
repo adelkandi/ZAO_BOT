7c0089ecd880ff66a1c41d57d250c74682efc75e
Adel Kandi <adelkandi68@gmail.com>
2023-02-14 16:32:53 +0100

    New Update

--- main.py
+++ main.py
@@ -3,23 +3,20 @@ import discord
 import os
 from dotenv import load_dotenv
 import requests  #  Make HTTPs requists to get DATA from API 
-import json    
+import json   
+import openai 
 
 
 #ZAO_BOT: 
 
 
 load_dotenv()   # Load the .env file to hide the TOKEN
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")    
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")   
+openai.api_key = os.getenv("API_Key")  # API_Key from OpenAI 
+
 intents= discord.Intents.default()
 intents.message_content = True
 BOT = discord.Client(intents=intents)     # make the Bot connect with Discord 
-
-
-# If an of discord user chat uses one of this on this list of words the bot will respone:
-Words_List = ["boring","bored","sad","miserable","nothing new","life isnt funny any more","depressed","depresstion","unhappy","looks boring"]
-
-
 
 
 # Helper function to get a Joke from the API 
@@ -28,6 +25,23 @@ def get_Joke():
     json_data = json.loads(response.text) # Convert the response to json file to store data
     Joke = json_data
     return(Joke['value'])
+
+
+
+def get_Dav_Joke():        #function to requist text-davinci-003
+    response = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = "Give me a random funny joke",
+        max_tokens = 50,
+        n = 1,
+        stop = None,
+        temperature = 0.5,
+        )
+    dav_Joke = response.choices[0].text.strip()
+    print("Joke data:",dav_Joke)   # to see on terminal and debug 
+    return dav_Joke
+
+
 
 def Update_jk(jk_message):               # Update and add new Local Jokes to json data file 
     with open("Data.json","r") as file:
@@ -68,7 +82,14 @@ async def on_message(message):
         return
     
     # User command reply :
-    if message.content.startswith('$jk'):
+    # with text-davenci-003:
+    if message.content.startswith('$jk/dav'):
+        print("openai api key:",openai.api_key)
+        Joke = get_Dav_Joke()
+        print("joke data:",Joke)
+        await message.channel.send(Joke)
+
+    if message.content.startswith('$jk/Norris'):
         Joke = get_Joke()
         await message.channel.send('Here is the joke: ')
         await message.channel.send(Joke)
